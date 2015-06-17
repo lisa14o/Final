4fe3921714d601dfce7d5e39e285a0e51fbc01bb
cluetrain <greenstrat67@gmail.com>
2015-06-16 17:44:13 -0700

    testing

---
+++ includes/final_testHeader.php
@@ -0,0 +1,28 @@
+<!--copy-->
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+	<title><?=$title?></title>
+	<link href=<?=$href?> type="text/css" rel="stylesheet"  >
+</head>
+<body>
+	<div id="wrapper">
+		<header> 
+			<nav>
+				<ul id="nav">
+					<li class="big"><a href="final_index.php">HOME</a></li>
+					<li class="big"><a href="final_music.php">MUSIC</a>
+						<ul>
+							<li><a href="final_empirePrism.php"></a></li>
+							<li><a href="final_lifeOn.php"></a></li>
+							<li><a href="final_handsAcross.php"></a></li>
+							<li><a href="final_foreignBody.php"></a></li>	
+						</ul>
+					</li>
+					<li class="big"><a href="final_tour.php">TOUR</a></li>
+					<li class="big"><a href="final_contact.php">CONTACT</a></li>
+				</ul>
+			</nav>
+		</header>
+		<div id="main">
